6af8b97c5267c91ad6f5df930e8436d6374e387c
idoshita <koshiroot@yahoo.co.jp>
2023-02-20 21:54:22 +0900

    変更した

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ env.read_env(os.path.join(BASE_DIR,".env"))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'b6o)t*=br&b1$_yaj+yrx$(9vqtswiohnys(+^v1dikc6d@7@2'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
